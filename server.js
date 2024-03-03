7dd870734aaf9c3f13a352174fa433db1d3cc85e
Irfan Ishtiaq <irfan.ishtiaq@fcec.sa>
2024-03-03 12:20:05 +0300

    a new change

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
-  origin: 'https://www.fcec.sa', // or use '*' to allow any origin
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  origin: '*', // or use '*' to allow any origin
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
 app.use(cors(corsOptions));
