aeaf30b1518f721a70a771e56d914fd49f97c81d
Irfan Ishtiaq <irfan.ishtiaq@fcec.sa>
2024-05-07 11:27:01 +0300

    new attributes

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 // Import necessary libraries
-const nodemailer = require('nodemailer');
+const { google } = require('googleapis');
 const cors = require('cors');
 const express = require('express');
 const multer = require('multer');
+const nodemailer = require('nodemailer');
 const mongoose = require('mongoose');
 const fs = require('fs');
 require('dotenv').config();
@@ -10,12 +11,28 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+const corsOptions = {
+  origin: 'https://www.fcec.sa',
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 // Setup multer for file uploads
 const upload = multer({ dest: 'uploads/' });
 
 const mongoUri = process.env.MONGO_URI;
+
+
+const credentials = JSON.parse(Buffer.from(process.env.GOOGLE_CREDENTIALS_BASE64, 'base64').toString('ascii'));
+
+// Configure JWT auth client
+const { client_email, private_key } = credentials;
+const auth = new google.auth.JWT(credentials.client_email, null, credentials.private_key, [
+  'https://www.googleapis.com/auth/spreadsheets',
+]);
+
+// Initialize the Sheets API
+const sheets = google.sheets({ version: 'v4', auth });
 
 // Connect to MongoDB
 mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -28,11 +45,12 @@ const applicationFormSchema = new mongoose.Schema({
   lastName: String,
   email: String,
   phone: String,
+  profession: String,
   address: String,
   education: [{
-    highestEducation: String,
-    fieldOfStudy: String,
-    institute: String,
+  highestEducation: String,
+  fieldOfStudy: String,
+  institute: String,
   }],
   experience: [{
     companyName: String,
@@ -43,13 +61,35 @@ const applicationFormSchema = new mongoose.Schema({
   resume: String,
 });
 
+
 const ApplicationForm = mongoose.model('ApplicationForm', applicationFormSchema);
 
 // Express middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Email transporter setup
+
+function flattenEducationEntries(entries) {
+  const flattened = [];
+  entries.forEach(entry => {
+    flattened.push(entry.highestEducation || "");
+    flattened.push(entry.fieldOfStudy || "");
+    flattened.push(entry.institute || "");
+  });
+  return flattened;
+}
+
+// Flatten Function for Experience Entries
+function flattenExperienceEntries(entries) {
+  const flattened = [];
+  entries.forEach(entry => {
+    flattened.push(entry.companyName || "");
+    flattened.push(entry.positionTitle || "");
+    flattened.push(entry.yearsOfExperience || "");
+  });
+  return flattened;
+}
+
 const transporter = nodemailer.createTransport({
   host: "smtp-mail.outlook.com",
   port: 587,
@@ -60,81 +100,101 @@ const transporter = nodemailer.createTransport({
   },
 });
 
+
 // Route to handle form submission
 app.post('/submit-form', upload.single('resume'), async (req, res) => {
-  // Extracting text fields; file is handled separately by Multer
-  const { firstName, lastName, email, phone, address, highestEducation, fieldOfStudy, institute, companyName, positionTitle, yearsOfExperience, skills } = req.body;
+  const { firstName, lastName, email, phone, profession, address, highestEducation, fieldOfStudy, institute, companyName, positionTitle, yearsOfExperience, skills } = req.body;
 
-  let encodedFile = null, resumeFilePath = '';
+  let encodedFile = null;
   if (req.file) {
-    const fileBuffer = fs.readFileSync(req.file.path);
+    const fileBuffer = await fs.promises.readFile(req.file.path);
     encodedFile = fileBuffer.toString('base64');
-    resumeFilePath = req.file.path; // Use the temporary file path for email attachment
-    // fs.unlinkSync(req.file.path); // Don't immediately clean up; we'll send it as an attachment
+    await fs.promises.unlink(req.file.path); // Clean up the uploaded file after processing
   }
 
-  const educationFormatted = highestEducation.map((educationLevel, index) => {
-    return `${educationLevel}, ${fieldOfStudy[index]}, ${institute[index]}`;
-  }).join('; ');
+  const skillsFormatted = Array.isArray(skills) ? skills.join(', ') : skills;
 
-  const experienceFormatted = companyName.map((company, index) => {
-    return `${company}, ${positionTitle[index]}, ${yearsOfExperience[index]} years`;
-  }).join('; ');
+  // Define educationEntries and experienceEntries before using them
+  const educationEntries = highestEducation.map((educationLevel, index) => ({
+    highestEducation: educationLevel,
+    fieldOfStudy: fieldOfStudy[index],
+    institute: institute[index],
+  }));
 
-  const applicationFormEntry = new ApplicationForm({
-    firstName,
-    lastName,
-    email,
-    phone,
-    address,
-    education: highestEducation.map((educationLevel, index) => ({
-      highestEducation: educationLevel,
-      fieldOfStudy: fieldOfStudy[index],
-      institute: institute[index],
-    })),
-    experience: companyName.map((company, index) => ({
-      companyName: company,
-      positionTitle: positionTitle[index],
-      yearsOfExperience: yearsOfExperience[index],
-    })),
-    skills: Array.isArray(skills) ? skills : [skills], // Ensure skills is always an array
-    resume: encodedFile,
-  });
+  const experienceEntries = companyName.map((company, index) => ({
+    companyName: company,
+    positionTitle: positionTitle[index],
+    yearsOfExperience: yearsOfExperience[index],
+  }));
 
   try {
-    await applicationFormEntry.save();
+    const applicationFormEntry = new ApplicationForm({
+      firstName,
+      lastName,
+      email,
+      phone,
+      profession,
+      address,
+      education: educationEntries,
+      experience: experienceEntries,
+      skills: skillsFormatted,
+      resume: encodedFile,
+    });
 
-    // Sending email notification
+    await applicationFormEntry.save();
+    
+    // Prepare data for Google Sheets
+    const rowData = [
+      [
+        firstName, lastName, email, phone, profession, address,
+        ...flattenEducationEntries(educationEntries),
+        ...flattenExperienceEntries(experienceEntries),
+        skillsFormatted
+      ],
+    ];
+    
+    const request = {
+      spreadsheetId: '1Rx7MQNJ262ohizPM2Wqw2wTsAOoD7oKWoaE-zrtvpG4',
+      range: 'Sheet1!A:M',
+      valueInputOption: 'RAW',
+      resource: { values: rowData },
+    };
+
+    await sheets.spreadsheets.values.append(request);
+
     const mailOptions = {
       from: process.env.OUTLOOK_EMAIL, // sender address
       to: 'irfan.ishtiaq@futurecityec.com', // replace with your email
-      subject: 'New Form Submission Received', // Subject line
-      text: `A new form has been submitted. Details:\n\nName: ${firstName} ${lastName}\nEmail: ${email}\nPhone: ${phone}\nAddress: ${address}\nEducation: ${educationFormatted}\nExperience: ${experienceFormatted}\nSkills: ${skills.join(', ')}`, // Customize as needed
-      attachments: [
-        {
-          filename: 'resume.pdf', // or '.docx' etc. depending on the file type
-          path: resumeFilePath // Path to the file
-        }
-      ]
+      subject: 'New Form Submission Notification', // Updated subject line
+      text: `A new form has been submitted. Please check the spreadsheet for details: https://docs.google.com/spreadsheets/d/1Rx7MQNJ262ohizPM2Wqw2wTsAOoD7oKWoaE-zrtvpG4`, // Link to the spreadsheet with your specific ID
     };
+    
 
+    // Send the email notification
     transporter.sendMail(mailOptions, function (error, info) {
       if (error) {
         console.log('Email send error:', error);
       } else {
         console.log('Email sent: ' + info.response);
       }
-      // Cleanup the uploaded file after sending the email
-      if(resumeFilePath) fs.unlinkSync(resumeFilePath);
     });
 
     res.send('Application submitted successfully.');
   } catch (error) {
-    console.error('Save to MongoDB failed:', error);
+    console.error('Error:', error);
     res.status(500).send('An error occurred.');
   }
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my application!');
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
 });
