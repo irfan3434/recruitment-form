e73a46ecaf0f750a4c041838a25996b957712ec1
Irfan Ishtiaq <irfan.ishtiaq@fcec.sa>
2024-12-02 13:34:41 +0300

    again trying to add the phone extension

--- server.js
+++ server.js
@@ -69,6 +69,9 @@ const transporter = nodemailer.createTransport({
 app.post('/submit-form', upload.single('resume'), async (req, res) => {
   const { firstName, lastName, email, phone, profession, address, highestEducation, fieldOfStudy, institute, companyName, positionTitle, yearsOfExperience, skills, jobPosition } = req.body;
 
+
+  const fullPhoneNumber = country_code ? `${country_code}${phone}` : phone;
+
   let encodedFile = null;
   if (req.file) {
     const fileBuffer = await fs.promises.readFile(req.file.path);
@@ -83,7 +86,7 @@ app.post('/submit-form', upload.single('resume'), async (req, res) => {
       firstName,
       lastName,
       email,
-      phone,
+      phone: fullPhoneNumber,
       profession,
       address,
       education: highestEducation.map((educationLevel, index) => ({
@@ -127,7 +130,7 @@ app.post('/submit-form', upload.single('resume'), async (req, res) => {
         <tr><td>First Name</td><td>${firstName}</td></tr>
         <tr><td>Last Name</td><td>${lastName}</td></tr>
         <tr><td>Email</td><td>${email}</td></tr>
-        <tr><td>Phone</td><td>${phone}</td></tr>
+        <tr><td>Phone</td><td>${fullPhoneNumber}</td></tr>
         <tr><td>Profession</td><td>${profession}</td></tr>
         <tr><td>Address</td><td>${address}</td></tr>
         <tr><td>Skills</td><td>${skillsFormatted}</td></tr>
